71dbbe42604d77166945ff77c5e64e27b72845c0
COE-14 <coe-14@COE-14s-iMac.local>
2020-01-24 15:08:07 +0530

    first commit

---
+++ FinalExamProject/dB/ManageDB.swift
@@ -0,0 +1,13 @@
+//
+//  ManageDB.swift
+//  FinalExamProject
+//
+//  Created by COE-14 on 24/01/20.
+//  Copyright © 2020 COE-14. All rights reserved.
+//
+
+import UIKit
+
+class ManageDB: NSObject {
+
+}
